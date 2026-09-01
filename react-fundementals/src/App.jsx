8bf875d3b90aa599a93fb46dda3b22fe34cfbe33
Adish Udayan <adishudayan62@gmail.com>
2026-09-01 13:05:31 +0530

    first commit

---
+++ react-fundementals/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Welcome to My App</h1>
+        <h2>This is a simple React app</h2>
+      </div>
+      <div>Chaacha</div>
+    </>
+  );
+};
+
+export default App;
