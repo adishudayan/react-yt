335e95601db0f3074b9fab0fd8e625d54c0392ba
Adish Udayan <adishudayan62@gmail.com>
2026-09-01 17:29:16 +0530

    props code

--- react-fundementals/src/App.jsx
+++ react-fundementals/src/App.jsx
@@ -1,10 +1,29 @@
-import Card from './components/card';
+import Card from './components/Card';
 import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
       <Navbar />
-      <Card />
+      <div className="parent">
+        <Card
+          title="My First Card"
+          description="This is my first card"
+          category="Healthcare"
+          img="https://plus.unsplash.com/premium_photo-1710117797127-624a10ec4e4f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <Card
+          title="My Second Card"
+          description="This is my second card"
+          category="Manufacturing"
+          img="https://plus.unsplash.com/premium_photo-1755883199872-2d31c8b8b012?q=80&w=1112&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <Card
+          title="My Third Card"
+          description="This is my third card"
+          category="Technology"
+          img="https://plus.unsplash.com/premium_photo-1710787193487-9e7f43c02551?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+      </div>
     </>
   );
 };
--- react-fundementals/src/components/Card.jsx
+++ react-fundementals/src/components/Card.jsx
@@ -1,11 +1,14 @@
-function Card() {
+const Card = (props) => {
   return (
     <>
       <div className="card">
-        <h1>Card Component</h1>
-        <p>This is a simple card component.</p>
+        <h1>{props.title}</h1>
+        <img src={props.img} alt="Placeholder" />
+        <p>{props.description}</p>
+        <span className="card-category">{props.category}</span>
+        <button className="card-button">Click Me</button>
       </div>
     </>
   );
-}
+};
 export default Card;
